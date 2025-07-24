faf551d4e4928d121179702fe0c7bf08872b4822
saadullah90-code <saadullahsiddiqui90@gmail.com>
2025-07-24 09:16:59 +0500

    Integrated SpeedInsights successfully

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
-import Layout from "@/components/common/Layout"
-import "@/styles/main.scss"
+// import Layout from "@/components/common/Layout"
+// import "@/styles/main.scss"
+// import { SpeedInsights } from "@vercel/speed-insights/react";
 
+
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <Layout>
+//       <Component {...pageProps} />
+//       <SpeedInsights debug />
+//     </Layout>
+   
+//   )
+// }
+import Layout from "@/components/common/Layout";
+import "@/styles/main.scss";
+import dynamic from "next/dynamic";
+
+// SpeedInsights ko dynamically import kar rahe hain taake client side pe hi render ho
+const SpeedInsights = dynamic(
+  () => import("@vercel/speed-insights/react").then((mod) => mod.SpeedInsights),
+  { ssr: false }
+);
 
 export default function App({ Component, pageProps }) {
+  console.log("SpeedInsights component loaded ✅");
+
   return (
     <Layout>
       <Component {...pageProps} />
+      <SpeedInsights debug />
     </Layout>
-  )
+  );
 }
